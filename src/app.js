36961dde22b219d80ebdfd7634f3f812211673e8
TheJeth <145587508+TheJeth@users.noreply.github.com>
2023-11-17 02:08:10 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,40 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.querySelector(".number").innerHTML = choices();
+  document.querySelector(".top").innerHTML = sameSymbol;
+  document.querySelector(".bottom").innerHTML = sameSymbol;
 };
+
+function choices() {
+  let ArrNumbers = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  let numbersIndex = Math.floor(Math.random() * ArrNumbers.length);
+  return ArrNumbers[numbersIndex];
+}
+
+function symbol() {
+  let ArrSymbol = ["♦", "♥", "♠", "♣"];
+  let symbolsIndex = Math.floor(Math.random() * ArrSymbol.length);
+  return ArrSymbol[symbolsIndex];
+}
+
+let sameSymbol = symbol();
